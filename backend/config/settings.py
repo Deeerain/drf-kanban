7dd9d7098a45c17ec57ee3af9cec9867ab2d8327
deerain <d33.i2.1n@gmail.com>
2023-07-02 20:05:40 +0300

    add requirements DRF, jwt ...

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,14 +1,18 @@
 from pathlib import Path
 from os import environ
+from datetime import timedelta
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = environ.get("SECRET_KEY")
+SECRET_KEY = environ.get("SECRET_KEY", "django")
 
 DEBUG = int(environ.get("DEBUG", 1))
 
 ALLOWED_HOSTS = environ.get("ALLOWED_HOSTS", "localhost").split()
+
+CORS_ALLOWED_ORIGINS = environ.get("ALLOWED_ORIGINS",
+                                   "http://localhost").split()
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -17,11 +21,20 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Сторонние приложения
+    "corsheaders",
+    "rest_framework",
+    "rest_framework_simplejwt",
+
+    # Локальные приложения
+    "kanban",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -57,7 +70,7 @@ DATABASES = {
         "PASSWORD": environ.get("DB_PASSWORD", "django"),
         "HOST": environ.get("DB_HOST", "localhost"),
         "PORT": environ.get("DB_HOST", 5432),
-    }
+    },
 }
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -91,3 +104,24 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# REST FRAMEWORK
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+}
+
+# SIMPLE JWT
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+}
