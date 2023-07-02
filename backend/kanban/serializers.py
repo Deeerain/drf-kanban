487812a0a1491fc064f729f8f6e08d7c44f48713
deerain <d33.i2.1n@gmail.com>
2023-07-02 21:10:25 +0300

    add serializer

---
+++ backend/kanban/serializers.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+from kanban.models import Board, Column, Task
+
+
+User = get_user_model
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('pk', 'username')
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = ('pk', 'name', 'created', 'updatd', 'user')
+
+
+class ColumnSeriaizer(serializers.ModelSerializer):
+    class Meta:
+        model = Column
+        fields = ('pk', 'name', 'board', 'created', 'updated')
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('pk', 'name', 'description', 'updated', 'created', 'user')
