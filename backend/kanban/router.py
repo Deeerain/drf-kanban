685a83b09ea0602c17104d5788030436da96a955
deerain <d33.i2.1n@gmail.com>
2023-07-07 16:35:27 +0300

    added tasks viewsets router

--- backend/kanban/router.py
+++ backend/kanban/router.py
@@ -7,6 +7,7 @@ router = routers.SimpleRouter()
 
 router.register('boards', viewsets.BoardViewset, 'boards')
 router.register('columns', viewsets.ColumnViewset, 'boards')
+router.register('tasks', viewsets.TaskViewset, 'tasks')
 router.register('users', viewsets.UserViewset, 'users')
 
 urlpatterns = router.urls
